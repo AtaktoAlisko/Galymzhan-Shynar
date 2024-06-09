1bfae5ac733739e6b0e7b24c65b12519a8ab81bd
Алибек Мурат <alibekmurat@MacBook-Pro-Alibek.local>
2024-06-09 12:33:30 +0500

    Init

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -4,7 +4,7 @@ import { YMaps, Map } from '@pbe/react-yandex-maps';
 const YandexMap = () => (
   <YMaps>
     <div className="text-3xl text-center mt-10 color font">
-    Сіздерге табуға жеңіл болі үшін!
+    Мерекелік кешке астындағы картаны <br/> қолдану арқылы бара аласыздар!
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '500px' }}>
         <Map 
           defaultState={{ center: [50.038475, 72.971471], zoom: 15 }}
